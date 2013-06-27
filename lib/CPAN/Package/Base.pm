8528981d3b92a2e31fb0eed9c5986a821c60f034
Ben Morrow <ben@morrow.me.uk>
2013-06-27 04:53:31 +0100

    Separate out Jail.
    Also make this runnable as not-root, using su/do where appropriate. As
    this gets more complicated I get more uncomfortable about the idea of
    running it as root unjailed, even on a build machine.

---
+++ lib/CPAN/Package/Base.pm
@@ -0,0 +1,33 @@
+package CPAN::Package::Base;
+
+use 5.010;
+use warnings;
+use strict;
+
+sub new {
+    my ($class, %atts) = @_;
+    bless \%atts, $class;
+}
+
+sub _set {
+    my ($self, %atts) = @_;
+    while (my ($k, $v) = each %atts) {
+        $self->{$k} = $v;
+    }
+    return $self;
+}
+
+sub _new {
+    my ($self, $class, @atts) = @_;
+    "CPAN::Package::$class"->new(
+        config  => $self->config,
+        @atts,
+    );
+}
+
+sub config {
+    @_ and return $_[0]{config}{$_[1]};
+    $_[0]{config} //= {};
+}
+
+1;
---
+++ lib/CPAN/Package/Jail.pm
@@ -0,0 +1,104 @@
+package CPAN::Package::Jail;
+
+use 5.010;
+use warnings;
+use strict;
+use autodie;
+
+use parent "CPAN::Package::Base";
+
+use File::Path      qw/make_path/;
+use File::Slurp     qw/read_dir write_file/;
+
+for my $m (qw/name jname root/) {
+    no strict "refs";
+    *$m = sub { $_[0]{$m} };
+}
+
+sub umount { @{ $_[0]{umount} } }
+
+sub su {
+    my ($self, @cmd) = @_;
+    $self->config("su")->(@cmd);
+}
+
+sub mount_tmpfs {
+    my ($self, $on) = @_;
+    $self->su("mkdir", "-p", $on);
+    $self->su("mount", "-t", "tmpfs", "-o", "mode=777", "tmpfs", $on);
+}
+
+sub mount_nullfs {
+    my ($self, $mode, $dir, $on) = @_;
+    $self->su("mkdir", "-p", $dir, $on);
+    $self->su("mount", "-t", "nullfs", "-$mode", $dir, $on);
+}
+
+sub jpath { "/cpan2pkg/$_[1]" }
+sub hpath { $_[0]->root . $_[0]->jpath($_[1]) }
+
+sub _injail_sh { <<'SH' }
+#!/bin/sh
+
+set -e
+
+export LC_ALL=C TZ=UTC
+
+export PERL5_CPAN_IS_RUNNING=$$
+export PERL5_CPANPLUS_IS_RUNNING=$$
+export PERL_MM_USE_DEFAULT=1
+
+dir="/cpan2pkg/$1"
+shift
+
+echo "===> Running [$*] in [$dir]"
+
+cd "$dir"
+exec "$@"
+SH
+
+sub start {
+    my ($self) = @_;
+
+    $self->su("poudriere", "jail", "-sj", $self->name);
+    my $jname = $self->name . "-default";
+    chomp(my $root = qx/jls -j $jname path/);
+    $self->_set(jname => $jname, root => $root);
+
+    $self->mount_tmpfs($self->hpath(""));
+    mkdir $self->hpath("build");
+
+    my $pkg = $self->config("packages") . "/" . $self->jname;
+    $self->mount_nullfs("w", $pkg, $self->hpath("pkg"));
+    $self->_set(pkg => $pkg, umount => ["pkg", ""]);
+
+    write_file $self->hpath("injail"), $self->_injail_sh;
+
+    return $self;
+}
+
+sub injail {
+    my ($self, $dir, @cmd) = @_;
+    $self->su("jexec", $self->jname, 
+        "/bin/sh", $self->jpath("injail"), $dir, 
+        @cmd);
+}
+
+sub add_initial_pkgs {
+    my ($self) = @_;
+
+    $self->injail(".", "tar", "-xvf", "/packages/Latest/pkg.txz", 
+        "-s,/.*/,,", "*/pkg-static");
+    $self->injail(".", "./pkg-static", "add", $self->config("perlpkg"));
+}
+
+sub stop {
+    my ($self) = @_;
+
+    for (map $self->hpath($_), $self->umount) {
+        $self->su("umount", $_);
+    }
+    $self->su("poudriere", "jail", "-kj", $self->name);
+}
+
+1;
