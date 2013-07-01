1849735f17f67297973655f286c948fcc39a34b4
Ben Morrow <ben@morrow.me.uk>
2013-07-01 20:51:56 +0100

    Rename ::Config to CPAN::Package.
    Since this is now the starting-point for creating objects, representing
    the whole build session, it seems more appropriate.

--- lib/CPAN/Package.pm
+++ lib/CPAN/Package.pm
@@ -4,6 +4,45 @@ use 5.010;
 use warnings;
 use strict;
 
-our $VERSION = "0";
+our $VERSION = "1";
+
+use Carp;
+use HTTP::Tiny;
+use Module::Load    qw/load/;
+use Scalar::Util    qw/blessed/;
+
+for my $a (qw/ 
+    jail perl perlpkg
+    cpan metadb dist packages
+    su http
+/) {
+    no strict "refs";
+    *$a = sub { $_[0]{$a} };
+}
+
+sub new {
+    my ($class, %conf) = @_;
+
+    $conf{perl}     //= "/usr/bin/perl";
+    $conf{cpan}     //= "http://search.cpan.org/CPAN";
+    $conf{metadb}   //= "http://cpanmetadb.plackperl.org/v1.0/package";
+    $conf{su}       //= sub { system @_ };
+    $conf{http}     //= HTTP::Tiny->new;
+
+    bless \%conf, $class;
+}
+
+sub find {
+    my ($self, $type, @args) = @_;
+
+    my $root = blessed $self
+        or croak "->find is an object method";
+    my $class = "CPAN::Package::$type";
+    load $class;
+    $class->new(
+        config  => $self,
+        $class->find($self, @args),
+    );
+}
 
 1;
